13d40fd40118156bb9ff4db2b4d47736ddaa43f3
Mansoor Majeed <83159118+Mans22r@users.noreply.github.com>
2021-06-07 01:35:51 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+  <div className="note">
+    <h1>JavaScript and React.js</h1>
+    <p>
+      This was a wonderful 7 Day Bootcamp provided by Shaurya sinha sir.
+      we covered all the basics from Scartch including JavaScript, React.js ,HTML.
+      The session was really understanding and clear.
+      </p>
+      </div>
+  );
+}
+export default Note;
